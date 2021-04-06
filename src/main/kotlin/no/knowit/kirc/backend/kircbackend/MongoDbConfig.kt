a7988c8fb52d1b1f5868aed57808bf99e058bd8f
Tom Fagerland <tom.fagerland@entur.org>
2021-04-06 18:01:30 +0200

    Retry ZDT fix

--- src/main/kotlin/no/knowit/kirc/backend/kircbackend/MongoDbConfig.kt
+++ src/main/kotlin/no/knowit/kirc/backend/kircbackend/MongoDbConfig.kt
@@ -3,15 +3,9 @@ package no.knowit.kirc.backend.kircbackend
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.convert.converter.Converter
-import org.springframework.stereotype.Component
-import java.time.ZoneOffset
-import java.time.ZonedDateTime
-import java.util.Date
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions
-
+import java.time.ZonedDateTime
 import java.util.ArrayList
-
-
 
 
 @Configuration
@@ -21,18 +15,12 @@ class MongoDbConfig {
     fun customConversions(): MongoCustomConversions {
         val converters: MutableList<Converter<*, *>> = ArrayList()
         converters.add(ZonedDateTimeReadConverter())
-        converters.add(ZonedDateTimeWriteConverter())
         return MongoCustomConversions(converters)
     }
 }
-class ZonedDateTimeReadConverter : Converter<Date, ZonedDateTime> {
-    override fun convert(date: Date): ZonedDateTime {
-        return date.toInstant().atZone(ZoneOffset.UTC)
+class ZonedDateTimeReadConverter : Converter<String, ZonedDateTime> {
+    override fun convert(date: String): ZonedDateTime {
+        return ZonedDateTime.parse(date)
     }
-}
 
-class ZonedDateTimeWriteConverter : Converter<ZonedDateTime, Date> {
-    override fun convert(zonedDateTime: ZonedDateTime): Date {
-        return Date.from(zonedDateTime.toInstant())
-    }
 }
